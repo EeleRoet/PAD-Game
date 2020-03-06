a5704f899f473f92fa87a3deedfca99811c70be0
EeleRoet <eele.roet@hotmail.nl>
2020-03-06 15:38:43 +0100

    player laser fire scripts

--- PAD-Prototype/Assets/FireScript.cs
+++ PAD-Prototype/Assets/FireScript.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class FireScript : MonoBehaviour
 {
+    public Rigidbody2D laserBody;
+    public GameObject laser;
+
+    int lifetime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        laserBody = GetComponent<Rigidbody2D>();
+       
+        laser.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CheckTimer();
+    }
+
+   private void CheckTimer()
+    {
+        if( lifetime < 0)
+        {
+            laser.SetActive(false);
+        }
+        else
+        {
+            lifetime--;
+        }
+    }
+
+    public void Fire(int helling)
+    {
+        laser.SetActive(true);
+        lifetime = 200;
+
         
+        float afstand = Screen.width / 2;
+        float hoogte = afstand * helling;
+        float hoek = Mathf.Atan(hoogte / afstand);
+
+        laserBody.SetRotation(hoek * Mathf.Rad2Deg + 90);
+
     }
 }
---
+++ PAD-Prototype/Assets/ShootScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootScript : MonoBehaviour
+{
+
+    public Rigidbody2D playerBody;
+    // public FireScript fireScript;
+    public GameObject laser;
+    private FireScript script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+        script = laser.GetComponent<FireScript>();
+       // fireScript = GetComponent<FireScript>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot(1);
+    }
+
+    public void Shoot(int helling)
+    {
+        float afstand = Screen.width / 2;
+        float hoogte = afstand * helling;
+        float hoek = Mathf.Atan(hoogte / afstand);
+
+        playerBody.SetRotation(hoek * Mathf.Rad2Deg + 90);
+
+
+        script.Fire(helling);
+    }
+}
